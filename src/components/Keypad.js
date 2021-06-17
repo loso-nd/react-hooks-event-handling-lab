7890093104e07c01542da320a23dd6e19527213e
Nahum <losond8@gmail.com>
2021-06-16 22:56:30 -0600

    Working with event handlers by passing them as props and giving them cb functions.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
-// Code Keypad Component Here
+import React from "react"
+
+function Keypad(){
+
+    const handleChange = () =>{
+        console.log("Entering password...")
+    }
+    return (
+        <>
+        <label>Password</label>
+        <input 
+            onChange={handleChange}
+            type="password" />
+        </>
+    )
+}
+
+export default Keypad
